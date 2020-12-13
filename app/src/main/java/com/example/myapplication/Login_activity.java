989afec3427d528cc239d1fbbc2d275c4841c4e6
AmanChaudharyy <Achaudhary@youngman.co.in>
2020-12-13 13:04:03 +0530

    Login page

---
+++ app/src/main/java/com/example/myapplication/Login_activity.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Login_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_activity);
+    }
+
+    public void go_to_dashboard(View view) {
+        Intent intent = new Intent(Login_activity.this , Dashboard.class);
+        startActivity(intent);
+    }
+}

8f9079a2586ae7b81e0f690ac316f01a412a4c5e
AmanChaudharyy <Achaudhary@youngman.co.in>
2020-12-13 20:29:12 +0530

    splash_screen intailize

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -4,25 +4,36 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView textView ;
+    private static int SPLASH_SCREEN_TIME_OUT=2000; //After completion of 2000 ms, the next activity will get started.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //This method is used so that your splash activity
+        //can cover the entire screen.
         setContentView(R.layout.activity_main);
-
-        textView = findViewById(R.id.splash_title);
-        textView.setOnClickListener(new View.OnClickListener() {
+        //this will bind your activity_splash_screen.class file with activity_main.
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                Intent  intent = new Intent(MainActivity.this , Login_activity.class);
-                startActivity(intent);
+            public void run() {
+                Intent i=new Intent(MainActivity.this,
+                        Login_activity.class);
+                //Intent is used to switch from one activity to another.
+
+                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
             }
-        });
+        }, SPLASH_SCREEN_TIME_OUT);
     }
 
 }

7fc5a585423c252510f7319fe6aa93c3a78d8750
AmanChaudharyy <Achaudhary@youngman.co.in>
2021-01-07 15:02:54 +0530

    setup login page to backend via volley

--- app/src/main/java/com/example/myapplication/Challanstatus.java
+++ app/src/main/java/com/example/myapplication/Challanstatus.java
@@ -5,16 +5,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-import java.io.ByteArrayOutputStream;
+
+import com.example.myapplication.ScaffChallanstatus;
 
 public class Challanstatus extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int FILE_SELECT_CODE = 0;  // used in selecting file
     ImageView imageView;
     Bitmap imageBitmap;
 
@@ -35,20 +39,50 @@ public class Challanstatus extends AppCompatActivity {
         }
     }
 
+    public  void selectvideo(View view){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("*/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        try {
+            startActivityForResult(
+                    Intent.createChooser(intent, "Select a File to Upload"),
+                    FILE_SELECT_CODE);
+        } catch (android.content.ActivityNotFoundException ex) {
+            // Potentially direct the user to the Market with a Dialog
+            Toast.makeText(this, "Please install a File Manager.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
     @Override   // used to show it on Imageframe
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+        switch (requestCode) {
+            case FILE_SELECT_CODE:
+                if (resultCode == RESULT_OK) {
+                    // Get the Uri of the selected file
+                    Uri uri = data.getData();
 
-            Bundle extras = data.getExtras();
-            imageBitmap = (Bitmap) extras.get("data"); // get image bitmap data
+                    /* Passing ImageURI to the Second Activity */
+                    Intent intent = new Intent(this, Previewscreen.class);
+                    intent.setData(uri);
+                    intent.putExtra("data", 2);
+                    startActivity(intent);
+                }
+                break;
+            case REQUEST_IMAGE_CAPTURE:
+                if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
 
-            Intent intent = new Intent(this, Previewscreen.class);
-            intent.putExtra("image",imageBitmap); // send to another activity
-            intent.putExtra("data", 1);
-            startActivity(intent);
+                    Bundle extras = data.getExtras();
+                    Bitmap    imageBitmap = (Bitmap) extras.get("data"); // get image bitmap data
 
+                    Intent intent = new Intent(this, Previewscreen.class);
+                    intent.putExtra("image",imageBitmap); // send to another activity
+                    intent.putExtra("data", 1);
+                    startActivity(intent);
+
+                }
         }
-
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/com/example/myapplication/Login_activity.java
+++ app/src/main/java/com/example/myapplication/Login_activity.java
@@ -5,17 +5,90 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.zip.Inflater;
 
 public class Login_activity extends AppCompatActivity {
+
+    EditText UsernameEt, PasswordEt;
+    String url = "http://192.168.0.40:8000/loginfvfd";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_activity);
+
+        UsernameEt = (EditText)findViewById(R.id.User_email);
+        PasswordEt = (EditText)findViewById(R.id.UserPassword);
+    }
+    public void OnLogin(View view) {
+
     }
 
     public void go_to_dashboard(View view) {
-        Intent intent = new Intent(Login_activity.this , Dashboard.class);
-        startActivity(intent);
+        String username = UsernameEt.getText().toString();
+        String password = PasswordEt.getText().toString();
+        String type = "login";
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url ="https://74f0855c2b5d.ngrok.io/loginauth?user="+ username+"&password="+password ;
+
+// Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string
+
+                        try {
+                            JSONArray obj = new JSONArray(response);
+                            for(int i=0; i < obj.length(); i++) {
+                                JSONObject jsonobject = obj.getJSONObject(i);
+                                String name       = jsonobject.getString("name");
+                                Toast.makeText(getApplicationContext(),name,Toast.LENGTH_LONG).show();
+                                Intent intent = new Intent(Login_activity.this , Dashboard.class);
+                                startActivity(intent);
+                            }
+
+                        } catch (JSONException e) {
+                          //  e.printStackTrace();
+                            Toast.makeText(getApplicationContext(),"NOT AUTHENTICATED",Toast.LENGTH_LONG).show();
+
+                        }
+
+
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+                Toast.makeText(getApplicationContext(),type,Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+// Add the request to the RequestQueue.
+        queue.add(stringRequest);
+
     }
 }
